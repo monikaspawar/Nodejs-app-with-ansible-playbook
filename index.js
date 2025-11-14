ee8fa4f15e4c76515d3297026691db3379ea0a72
monikaspawar <monikaspawar1@gmail.com>
2025-11-14 15:23:27 +0530

    Replace index.js with simple Node.js application

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-const express = require("express");
-const path = require("path");
-const os = require('os');
+const http = require('http');
 
-const app = express();
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.render("index");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello World!</h1><p>Simple Node.js Application</p>');
 });
 
-app.get('/path', (req, res) => {
-  const hostname = os.hostname();
-  const networkInterfaces = os.networkInterfaces();
-  let ipAddress = 'Not found';
-
-  for (const interfaceName in networkInterfaces) {
-    const interfaces = networkInterfaces[interfaceName];
-    for (const iface of interfaces) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        ipAddress = iface.address;
-        break;
-      }
-    }
-  }
-  res.send(`Hostname: ${hostname}, IP Address: ${ipAddress}`);
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
